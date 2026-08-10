c40ef94381a569c7f126466e94e6fb5e7eecb75a
Big Pac <asherstaana@gmail.com>
2026-08-11 03:19:11 +0800

    change url in script

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 async function counterAPI() {
-    const api = await fetch("https://d1q5ag8e6d.execute-api.ap-southeast-1.amazonaws.com/count");
+    const api = await fetch("https://fnrr9hhx7a.execute-api.ap-southeast-1.amazonaws.com/count");
     const data = await api.json();
     document.getElementById("counterr").textContent = data.count;
 }
